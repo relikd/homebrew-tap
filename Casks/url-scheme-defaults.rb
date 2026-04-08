b6ea8c4bbd1ae5922d7c5426aa0bbf86d41378a2
relikd <info@relikd.de>
2026-04-08 21:03:47 +0200

    fix: url issues

--- Casks/url-scheme-defaults.rb
+++ Casks/url-scheme-defaults.rb
@@ -2,7 +2,7 @@ cask "url-scheme-defaults" do
   version "1.1.0"
   sha256 "fe8422032ebf569de74b51f61954ef64fdbe71ada51e3071a2a6ec921e4f79f4"
 
-  url "https://github.com/relikd/URL Scheme Defaults/releases/download/v#{version}/URL-Scheme-Defaults_#{version}.zip"
+  url "https://github.com/relikd/URL-Scheme-Defaults/releases/download/v#{version}/URL-Scheme-Defaults_#{version}.zip"
   name "URL Scheme Defaults"
   desc "Change the default macOS application for a given URL scheme (http:, feed:, etc.)."
   homepage "https://github.com/relikd/URL-Scheme-Defaults"
