31b3f96661e88a50ed02593e74d8070e059648b0
relikd <info@relikd.de>
2026-04-09 13:51:01 +0200

    chore: bump version for url-scheme-defaults

--- Casks/url-scheme-defaults.rb
+++ Casks/url-scheme-defaults.rb
@@ -1,6 +1,6 @@
 cask "url-scheme-defaults" do
-  version "1.1.0"
-  sha256 "fe8422032ebf569de74b51f61954ef64fdbe71ada51e3071a2a6ec921e4f79f4"
+  version "1.2.0"
+  sha256 "52c0dd15ab20f6ebdba829fb62e29d322fde1cfcbeed8124f7271583c9652541"
 
   url "https://github.com/relikd/URL-Scheme-Defaults/releases/download/v#{version}/URL-Scheme-Defaults_#{version}.zip"
   name "URL Scheme Defaults"
