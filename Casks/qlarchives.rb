35b3b1a35c2e24ab60df9ea49dab2cb1701489f0
relikd <info@relikd.de>
2026-04-15 20:29:45 +0200

    chore: bump version for qlarchives

--- Casks/qlarchives.rb
+++ Casks/qlarchives.rb
@@ -1,6 +1,6 @@
 cask "qlarchives" do
-  version "1.2.0"
-  sha256 "c7231304bbe2dce0b7d336c838aa0f3f9457dcd6efcb80b0ad5a98d9d396be48"
+  version "1.3.0"
+  sha256 "8a780e469e6756cb4f09a9657285c05c7e290dcd617ad011f2308c3dedb7a2a7"
 
   url "https://github.com/relikd/QLArchives/releases/download/v#{version}/QLArchives_#{version}.zip"
   name "QLArchives"
