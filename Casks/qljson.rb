824ef7a87f7ff64d6ad4c247385068c7ded2fe51
relikd <info@relikd.de>
2026-04-20 13:51:51 +0200

    chore: bump version for qljson

--- Casks/qljson.rb
+++ Casks/qljson.rb
@@ -1,6 +1,6 @@
 cask "qljson" do
-  version "1.0.0"
-  sha256 "0062e2fe6e2add86f79fca9de9323fea42835fc5d0b0fedaf82080050d9efebf"
+  version "1.1.0"
+  sha256 "afcb1a7ddedc03d9fc1d934b5a779a054e71481cfb04206be2260054c996a6f1"
 
   url "https://github.com/relikd/QLJSON/releases/download/v#{version}/QLJSON_#{version}.zip"
   name "QLJSON"
