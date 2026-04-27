2018bb3e8c12db0a997a08eed8407f3f0a85dc4b
relikd <info@relikd.de>
2026-04-27 20:06:29 +0200

    chore: bump version for relikd-qlmarkdown

--- Casks/relikd-qlmarkdown.rb
+++ Casks/relikd-qlmarkdown.rb
@@ -1,6 +1,6 @@
 cask "relikd-qlmarkdown" do
-  version "1.1.1"
-  sha256 "79de7977cb02a02b51d217037aa9b9f49d29dac4e1afef7c7a59a48962c63476"
+  version "1.1.2"
+  sha256 "0af59a473d545e083cbddff78f90ab16c34328468349b18b884ff9a1ab11c407"
 
   url "https://github.com/relikd/QLMarkdown/releases/download/v#{version}/QLMarkdown_#{version}.zip"
   name "QLMarkdown"
