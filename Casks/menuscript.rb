f25bd2835bd582d56b9ff58cc46b88c4f0f2fca1
relikd <info@relikd.de>
2026-09-14 11:58:39 +0200

    chore: bump version for menuscript

--- Casks/menuscript.rb
+++ Casks/menuscript.rb
@@ -1,6 +1,6 @@
 cask "menuscript" do
-  version "1.1.0"
-  sha256 "710643ac501b1e6919bd735b791ee68e5470d136c987a67210139f2c49bcdc52"
+  version "1.2.0"
+  sha256 "6637cad28453dd96282e991cc8cafa82e4f15e29cea72393c696411a3b3af36f"
 
   url "https://github.com/relikd/Menuscript/releases/download/v#{version}/Menuscript_#{version}.tar.gz"
   name "Menuscript"
